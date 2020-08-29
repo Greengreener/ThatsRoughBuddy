f7b1b7367e96fa318704e8f800720b141b01df1b
tek-f <aaron.falco2@gmail.com>
2020-08-29 13:46:26 +1000

    Update DeathDisplay.cs
    Added Update() to have the players distance be displayed after death by counting up from 0

--- Assets/Scripts/DeathDisplay.cs
+++ Assets/Scripts/DeathDisplay.cs
@@ -9,11 +9,20 @@ public class DeathDisplay : MonoBehaviour
     GameObject HUDPanel;//the players HUD display panel, set inactive on start
     [SerializeField]
     Text gems, distance;//text elements to display the distance travelled and the number of gems collected
+    int distanceDisplay;//used to display the players distance, counting up from 0 after their death
     void Start()
     {
         HUDPanel.SetActive(false);//set HUD Panel inactive
         //set the text elements to display the players score
         gems.text = "Gems: " + PlayerHandler.gems.ToString();
-        distance.text = "Distance: " + PlayerHandler.distance.ToString();
+        distanceDisplay = 0;
+    }
+    private void Update()
+    {
+        if(distanceDisplay < PlayerHandler.distance)//if distanceDisplay is less than the players travelled distance
+        {
+            distanceDisplay++;//increase distance display
+            distance.text = "Distance: " + distanceDisplay.ToString();//set text to equal distance display
+        }
     }
 }
